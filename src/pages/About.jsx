d3d1f247eaad8b8b68558cd3535a9509302ab75d
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-11-25 12:29:30 +0100

    code refacer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,10 +2,8 @@ export default function About() {
 
     return (
         <>
-
             <h1>About Us</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla soluta tempora eveniet vel. Quibusdam sed, facilis eum consequuntur debitis quo illo sequi ut voluptates tempore ipsa nobis! Veritatis, ad rem?</p>
-
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,8 @@ export default function Home() {
 
     return (
         <>
-
             <h1>Home Page</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla soluta tempora eveniet vel. Quibusdam sed, facilis eum consequuntur debitis quo illo sequi ut voluptates tempore ipsa nobis! Veritatis, ad rem?</p>
-
         </>
     )
 }
